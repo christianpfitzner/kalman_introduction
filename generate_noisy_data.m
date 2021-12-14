34b1758e02bc9303093e045ae8095190421eca2f
Christian Pfitzner <christian.pfitzner@evocortex.com>
2021-12-14 10:53:58 +0100

    Removing ; after end to be called from matlab.

--- generate_noisy_data.m
+++ generate_noisy_data.m
@@ -1,15 +1,5 @@
 
-% Funktion zur Erzeugung von Daten mit Pseudo-Rauschen
-%
-% Parameter:
-% A: Zustandsübergangsmodell nxn-Matrix
-% x: Zustand nx1-Vektor
-% vals: Anzahl an Messwerten
-% sigma: Standardabweichung des Rauschens
-% data: nx2-Matrix mit verrauschten Messwerten
-%
-% Autor: Stefan May
-% Datum: 15.02.2011
+
 
 function data=generate_noisy_data(A, x, vals, sigma)
 	for i=1:vals 
@@ -24,5 +14,5 @@ function data=generate_noisy_data(A, x, vals, sigma)
 		rnd1 = rand;
 		rnd2 = rand;
 		data(i,2) = x(2) + sigma * sqrt(-2.0 * log(rnd1)) * sin(2.0 * pi * rnd2);
-	end;
-end;
+	end
+end
